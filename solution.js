82ba5460891d1b54766c6c7e969050cba26e5e2d
ju8ko <julia.x.kobzar@gmail.com>
2022-07-05 15:06:11 +0200

    The exercise is ready

---
+++ solution.js
@@ -0,0 +1,19 @@
+//1.a.
+console.log("The lowest number is:", Math.min(-1, 4));
+//1.b.
+console.log("The highest number is:", Math.max(-1, 4));
+//2.a.
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(28.329));
+//2.b.
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+//3
+const random1_6 = Math.ceil(Math.random() * 6);
+console.log(random1_6);
